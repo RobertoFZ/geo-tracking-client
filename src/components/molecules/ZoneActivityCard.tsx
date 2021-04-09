dc13f34a701dda82cccc2afa91e91faad5f2e405
Roberto Franco <roberto.zetinca@outlook.com>
2021-04-09 17:53:13 -0500

    Add scroll to activity cards

--- src/components/molecules/ZoneActivityCard.tsx
+++ src/components/molecules/ZoneActivityCard.tsx
@@ -11,7 +11,7 @@ const ZoneActivityCard = ({
   activity
 }: IZoneActivityCard) => (
   <Card
-    style={{ margin: '0 10px' }}
+    style={{ margin: '0 10px', maxHeight: '230px', overflowY: 'scroll' }}
     title={activity.location_zone.name}
     size="small">
     <UserActivityList users={activity.users} />
